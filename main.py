e16d3d144b4a460a4952f5e473f33b769c0bf70b
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2026-02-04 21:19:07 +0545

    Fix model weights

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 
 # CONFIGURATION
 # 1. Put the path to your photo here
-IMAGE_PATH = "image.png"
+IMAGE_PATH = "image5.png"
 
 # 2. Path to your trained model
 MODEL_PATH = "best_emotion_model.keras"
